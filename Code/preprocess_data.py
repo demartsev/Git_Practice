b3948ad22953ed067e58cc94641b1f6019fd4237
Vlad <demartsev@gmail.com>
2020-12-03 11:12:29 +0100

    Revert "Changing_ys_to zs"
    This reverts commit f203a1d3fa3586f398da4ba657245ad9b7841ba4.

--- Code/preprocess_data.py
+++ Code/preprocess_data.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
 
-def get_kinematics(x, z):
+def get_kinematics(x, y):
 	dx = x - x.shift()
-	dz = z - z.shift()
+	dy = y - y.shift()
 	d2x = dx - dx.shift()
-	d2z = dz - dz.shift()
+	d2y = dy - dy.shift()
 
-	speed = np.sqrt(dx**2 + dz**2)
-	acceleration = np.sqrt(d2x**2 + d2z**2)
+	speed = np.sqrt(dx**2 + dy**2)
+	acceleration = np.sqrt(d2x**2 + d2y**2)
 
 	return speed, acceleration
 
